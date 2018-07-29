74ae94c485ae46b4e89b93c203d15cf2df770a8e
info-adk <13500129650@163.com>
2018-07-29 17:41:36 +0800

    Add files via upload

---
+++ ngram-liblinear/get_feature.py
@@ -0,0 +1,54 @@
+# coding:utf-8
+
+from sklearn.utils import shuffle
+from sklearn.datasets import dump_svmlight_file
+from sklearn.feature_extraction.text import CountVectorizer
+import os
+
+feature_dir = "/home/iiip/Info/capsnet_baseline/n-gram/data/feature(1,3).txt"
+ngram = CountVectorizer(ngram_range=(1, 3))
+
+
+# data是ngram表示特征的sparse matrix
+# label_list是每篇文档所属类别
+def get_dataset():
+    corpus = []
+    label_list = []
+    datadir = "/home/iiip/Info/capsnet/data/20_newsgroups_new"
+    for labelname in os.liostdir(datadir):
+        labeldir = os.path.join(datadir, labelname)
+        for textname in os.listdir(labeldir):
+            text = os.path.join(labeldir, textname)
+            with open(text, "r") as f:
+                content = f.read()
+                corpus.append(content)
+            label_list.append(labelname)
+    data = ngram.fit_transform(corpus)
+    return data, label_list
+
+
+# 将每篇文章所属类别转换成integer,即label_list -> lable
+def get_label(label_list):
+    label_list = label_list
+    label = []
+    label2index = {}
+    index = 0
+    datadir = "/home/iiip/Info/capsnet/data/20_newsgroups_new"
+    for labelname in os.listdir(datadir):
+        label2index[labelname] = index
+        index += 1
+    for i in label_list:
+        label.append(label2index[i])
+    return label
+
+
+def main():
+    if __name__ == "__main__":
+        data, label_list = get_dataset()
+        label = get_label(label_list)
+        data, label = shuffle(data, label)  # 打乱数据
+        print(ngram.ngram_range)
+        dump_svmlight_file(data, label, feature_dir)  # 将数据转换成libsvm格式并保存本地
+
+
+main()
---
+++ ngram-liblinear/predict.py
@@ -0,0 +1,33 @@
+# coding:utf-8
+
+from sklearn import metrics
+from liblinearutil import svm_read_problem,predict,problem,parameter,train
+
+feature_dir = "/home/iiip/Info/capsnet_baseline/n-gram/data/feature(1,1).txt"
+
+def metrics_result(actual, predict):
+    accuracy = round(metrics.accuracy_score(actual, predict), 3)
+    precision = round(metrics.precision_score(actual, predict,average="weighted"), 3)
+    recall = round(metrics.recall_score(actual, predict,average="weighted"), 3)
+    return accuracy,precision,recall
+
+
+def main():
+    if __name__ == "__main__":
+        y, x = svm_read_problem(feature_dir, return_scipy = True)
+        train_X = x[:16000]
+        train_y = y[:16000]
+        test_X = x[16000:]
+        test_y = y[16000:]
+
+        prob = problem(train_y,train_X)
+        param = parameter("-c 1 -s 2")
+        model = train(prob,param)
+        p_labs, p_acc, p_vals = predict(test_y, test_X, model)
+        accuracy, precision, recall = metrics_result(test_y,p_labs)
+        print
+        print "accuracy: ",accuracy
+        print "precision: ",precision
+        print "recall: ",recall
+
+main()
